71892dea462e8388f678a0c620bd2bf704982cde
Geoff Winans <gwinans@gmail.com>
2012-06-22 19:26:32 -0700

    Re-committing..

---
+++ analytics_backup_stage_1.py
@@ -0,0 +1,103 @@
+import os, os.path
+import sys
+import ConfigParser
+import urllib2 as ul
+import MySQLdb as mdb
+from ftplib import FTP
+
+# Test+Setup MySQL Connection
+
+def getConfig():
+	conf = ConfigParser.ConfigParser()
+	if not os.path.exists("./analytics_ftp.conf"):
+		conffile = open("./analytics_ftp.conf", "w")
+
+		backup_path = raw_input("Directory where files should be stored locally prior to remote push (trailing slash not required): ")
+
+		myhost = raw_input("Enter the MySQL Hostname: ")
+		myuser = raw_input("Enter the MySQL User: ")
+		mypass = raw_input("Enter the MySQL Password")
+		mydb = raw_input("Enter the name of the database: ")
+		myport = 3306
+
+		ftphost = raw_input("Enter the FTP Hostname: ")
+		ftpuser = raw_input("Enter the FTP Username: ")
+		ftppass = raw_input("Enter the FTP Password: ")
+		ftpport = 21
+
+		conf.add_section("tool")
+		conf.set("tool", "host", ftphost)
+		conf.set("tool", "port", ftpport)
+		conf.set("tool", "user", ftpuser)
+		conf.set("tool", "pass", ftppass)
+		conf.set("tool", "backup_path", backup_path)
+		conf.write(conffile)
+		print "You will need to edit the config file if your FTP or MySQL services run on alternative ports."
+		conffile.close()
+		print "Execute again to run the tool with the new configuration."
+		sys.exit(0)
+	else:
+		conf.read("./analytics_ftp.conf")
+		opts = conf.options("tool")
+		ftphost = conf.get("tool", "ftp_host")
+		ftpport = conf.getint("tool", "ftp_port")
+		ftpuser = conf.get("tool", "ftp_user")
+		ftppass = conf.get("tool", "ftp_pass")
+		myhost = conf.get("tool", "my_host")
+		myuser = conf.get("tool", "my_user")
+		mypass = conf.get("tool", "my_pass")
+		myport = conf.getint("tool", "my_port")
+		backup_path = conf.get("tool", "backup_path")
+
+	return { 'ftphost': ftphost, 'ftpport': ftpport, 'ftpuser': ftpuser, 'ftppass': ftppass, 'myhost': myhost, 'myuser': myuser, 'mypass': mypass, 'myport': myport, 'backup_path': backup_path }
+
+cfg = getConfig()
+
+try:
+	con = mdb.connect(cfg["my_host"], cfg["my_user"], cfg["my_pass"], cfg["my_db"])
+except:
+	print "Failed to connect to database."
+	print "Error: %d : %s" % (e.args[0], e.args[1])
+
+# Test+Setup FTP Connection.
+#try:
+#	ftp = FTP('ip.add.re.ss')
+#	ftp.login('someuser', 'somepass')
+#except:
+#	print "Error with FTP - %d : %s" % (e.args[0], e.args[1])
+
+
+# Now that we have a connection to the DB and FTP..
+# Get all the tables for visit_events_20* and media_events_20*
+# .. and dump them to .dat files
+cur = con.cursor()
+cur.execute("show tables like 'visit_events_20%'")
+ve = cur.fetchall()
+
+cur.execute("show tables like 'media_events_20%'")
+me = cur.fetchall()
+
+# Pop off the last two list dict elements (most recent archive tables, in case they may get late inserts)
+ve_slice = ve[:-2]
+me_slice = me[:-2]
+
+expected_files = len(me_slice) + len(ve_slice)
+
+for vevent in ve_slice:
+	cur.execute("SELECT * FROM " + vevent[0] + " INTO OUTFILE '" + backup_path + "\\" + vevent[0] + ".dat'")
+
+for mevent in me_slice:
+	cur.execute("SELECT * FROM " + mevent[0] + " INTO OUTFILE '" + backup_path + "\\" + mevent[0] + ".dat'")
+
+dat_count = len(glob.glob1(backup_path, "*.dat"))
+
+if dat_count == expected_files:
+	newpath = "/" + year + "/" + month
+	ftp.sendcmd("MKDIR " newpath)
+	ftp.cwd(newpath)
+	for item in os.listdir(cfg["backup_path"]):
+		f = open(item, "r")
+		ftp.storbinary("STOR "+ item +, f)
+		f.close()
+else:
+	print "Error: Total files in the directory do not match the total expected number of files."
---
+++ spider_funcs.py
@@ -0,0 +1,173 @@
+###
+
+def getConfig():
+	conf = ConfigParser.ConfigParser()
+	if not os.path.exists("./spider.conf"):
+		conffile = open("./spider.conf")
+		conf.add_section("Default")
+		conf.set("Default", "dbhost", "localhost")
+		conf.set("Default", "dbuser", "mysqlusername")
+		conf.set("Default", "dbpass", "mysqlpassword")
+		conf.set("Default", "dbname", "mysqldatabasename")
+		conf.set("Default", "log", "spider.log")
+		conf.set("Default", "urls-per-pass", 50)
+		conf.write(conffile)
+		conffile.close
+		logMessage("Config file created. Edit it and then re-run with --seed=http://your.starter.website.com/, add --verbose if you would like to see messages on-screen", log, verbose)
+		sys.exit(1)
+	else:
+		conf.read("./spider.conf")
+		opts = conf.options("Default")
+
+		log = conf.get("Default", "log")
+		perpass = conf.getint("Default", "urls-per-pass")
+		dbhost = conf.get("Default", "dbhost")
+		dbuser = conf.get("Default", "dbuser")
+		dbpass = conf.get("Default", "dbpass")
+		dbname = conf.get("Default", "dbname")
+		
+		return { 'log': log, 'perpass': perpass, 'dbhost': dbhost, 'dbuser': dbuser, 'dbpass': dbpass, 'dbname': dbname }
+
+###
+
+def createTable():
+	sql = "CREATE TABLE IF NOT EXISTS spider (  "
+	sql += "`id` int not null primary key auto_increment, `url` TEXT, url_hash char(32) UNIQUE,"
+	sql += " `parent_url` int, `status` tinyint, `content_size` int, `content` LONGTEXT, `http_code` int,"
+	sql += " `time_taken` int, `created` datetime, INDEX(`created`), INDEX(`url_hash`)"
+	sql += " ) engine=innodb"
+
+	cur = con.cursor()
+	cur.execute("SHOW TABLES LIKE 'spider'")
+	exists = cur.fetchone()
+	if len(exists) == 0:
+		# Determine if we are running MySQL 5.5 and if innodb_file_per_table is set
+		cur.execute("SELECT VERSION()")
+		ver = cur.fetchone()
+
+		# Determine if innodb_file_per_table is ON or not.
+		cur.execute("SHOW GLOBAL VARIABLES LIKE 'innodb_file_per_table'")
+		fpt = cur.fetchone()
+
+		# Check if mysql version and configuration is capable of handling compressed rows
+		if ver["version()"].startswith("5.5") AND fpt["Value"] == "ON":
+			logMessage("Detected MySQL 5.5 with innodb_file_per_table. Adding row compression.", log, verbose)
+			sql += " row_format=compressed key_block_size=8"
+
+		# Create the table
+		cur.execute(sql)
+		con.commit()
+	else:
+		logMessage("Table exists.", log, verbose)
+
+###
+
+def addSeed(seed):
+	# Stuff it in the DB and fetch it prior to moving on.
+	cur = con.cursor()
+	cur.execute("TRUNCATE TABLE `spider`") 
+	cur.execute("INSERT INTO `spider` VALUES (NULL, %s, %s, '', 0, 0, 0, 0, NOW(), 1)", (seed, md5.md5(seed).hexdigest()))
+	content = getContentFromURL(seed)
+	urls = extractURLs(content)
+	cur.execute("SELECT id FROM spider WHERE url_hash = %s", (md5.md5(parent).hexdigest()))
+	parentId = cur.fetchone()
+	for items in urls:
+		cur.execute("UPDATE `spider` SET `parent_url` = %d, `content` = %s, `content_length` = %d, `status` = 1 WHERE `url_hash` = %s", (parentId["id"], content, len(content), md5.md5(seed).hexdigest()))
+
+	con.commit()
+
+###
+
+def logMessage(message, log, verbose):
+	fmessage = "["
+	fmessage += strftime("%Y-%m-%d %H:%M:%S")
+	fmessage += "] " + message + "\n"
+
+	if log != "":
+		lfile = open(log, "w")
+		lfile.writelines(fmessage)
+		lfile.close
+
+	if verbose:
+		print fmessage
+
+###
+
+def dbHasContent():
+	# This is where we assume that we have a seed url if count != 0 
+	cur = con.cursor()
+	cur.execute("SELECT COUNT(*) as cnt FROM `spider`")
+	data = cur.fetchone()
+
+	if data[0] > 0:
+		return True
+	else:
+		return False
+
+###
+
+def getURLsFromDb(limit=5):
+	# Get URLs from DB, limited to limit
+	cur = con.cursor()
+	cur.execute("SELECT `url` FROM `spider` WHERE `status` = 0 LIMIT %d", (limit))
+	rows = cur.fetchall()
+	cur.execute("UPDATE `urls` SET `status` = 1 WHERE `id` = %d", (",".join(i["id"])) )
+	con.commit()
+	return rows
+
+###
+
+def getContentFromURL(url):
+	# Get the content, return dict with content + time_taken (in millis)
+	req = ul.Request(url)
+	start = time.time() * 1000
+	response = ul.urlopen(req)
+
+	logMessage("Retrieving content from " + url, log, verbose)
+
+	content = response.read()
+	code = response.getcode()
+	end = time.time() * 1000
+
+	time_taken = int(round(end - start))
+
+	logMessage("Content retrieved in " + time_taken + "ms, code: " + code, log, verbose)
+
+	return { 'content': content, 'http_code': code, 'time_taken': time_taken }
+
+###
+
+def extractURLs(content):
+	urls = []
+	soup = BeautifulSoup(content)
+	for link in soup.find_all('a'):
+		urls.append(link.get('href'))
+
+	uniq = set(urls) #Yay, uniqueness just by changing the type from a list to a set!
+	fixedlist = []
+
+	# filter our list of URLs down to items that begin with http
+	for url in uniq:
+		strurl = str(url)
+		if strurl.startswith('http'):
+			fixedlist.append(strurl)
+
+	return fixedlist
+
+###
+
+def insertContent(urllist, parent):
+	cur = con.cursor()
+	cur.execute("SELECT id FROM spider WHERE url_hash = %s", (md5.md5(parent).hexdigest()))
+	parentId = cur.fetchone()
+	for item in urllist:
+		cur.execute("INSERT INTO `spider` (id, url, url_hash, parent_url, created) VALUES (NULL, %s, %s, %d, NOW())", (item, md5.md5(item).hexdigest(), parentId["id"]))
+	con.commit()
+
+###
+
+def updateURL(url, content, http_code, time_taken):
+	cur = con.cursor()
+	cur.execute("UPDATE `spider` SET `content` = %s, `content_size` = %d, `http_code` = %d, `time_taken` = %d WHERE `url` = %s", (content, len(content), http_code, time_taken, md5.md5(url).hexdigest()))
+	con.commit()
+	logMessage("Updated " + url + " - Content Length: " + len(content) + ", HTTP Code: " + http_code + ", Time Taken: " + time_taken + "ms", log, verbose)
